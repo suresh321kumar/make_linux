0d003b4ae92259f45c460e396f379c7efd3249c5
Kompile <kompile@packer-debian-8-amd64.droplet.local>
2020-05-15 07:59:09 +0000

    working

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+config_file=/home/kompile/suresh_dir/linux_dev/config_files/mipsel.miniconf
+
+cd linux-4.14.176
+echo "==== Cleaning"
+make clean && make mrproper
+rm vmlinux.obj vmlinux.lzma firmware.bin
+echo "==== Compiling kernel"
+make ARCH=mips allnoconfig KCONFIG_ALLCONFIG=${config_file}
+make ARCH=mips CROSS_COMPILE=/home/kompile/mipsel-linux-muslsf-cross/bin/mipsel-linux-muslsf-
+echo "==== DONE"
---
+++ image.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "==== Building image"
+cd /home/kompile/suresh_dir/linux_dev/linux-4.14.176
+/home/kompile/mipsel-linux-muslsf-cross/bin/mipsel-linux-muslsf-objcopy -O binary -R .reginfo -R .notes -R .note -R .comment -R .mdebug -R .note.gnu.build.id -S vmlinux vmlinux.obj
+cat /home/kompile/suresh_dir/linux_dev/image-mt7628an_kloud9_br325.dtb >> vmlinux.obj
+/home/kompile/suresh_dir/linux_dev/lzma e vmlinux.obj -lc1 -lp2 -pb2 vmlinux.lzma
+/usr/bin/mkimage -A mips -O linux -T kernel -C lzma -a 0x80000000 -e 0x80000000 -n 'br325.kloud9.in (test)' -d vmlinux.lzma firmware.bin
+cp firmware.bin /home/kompile/suresh_dir/linux_dev/output/
+echo "==== Done"
